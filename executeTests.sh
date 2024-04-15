e3e48343b076f6ea81cbc76504efa354d76b14b4
DEVSOG12 <oreofesolarin@gmail.com>
2024-04-15 17:23:10 -0400

    Done with tests

--- executeTests.sh
+++ executeTests.sh
@@ -1,8 +1,15 @@
 # This will run our compiler on a C file, export its LLVM ASM to a .ll file and output how it went to the log.
 # The LLVM ASM is than ran and its outut sent to the execution log.
 function runTest {
-    ./run.sh -i tests/$1.c -fAsm -o tests/$1.ll &> tests/$1.log
+    ./run.sh -i tests/$1.py -fAsm -o tests/$1.ll &> tests/$1.log
     lli tests/$1.ll &> tests/$1Execution.log
+    python3 tests/$1.py &> tests/$1Python.log
+    diff tests/$1Execution.log tests/$1Python.log
+    if [ $? -eq 0 ]; then
+        echo "Test $1 passed"
+    else
+        echo "Test $1 failed"
+    fi
 }
 
 # Run all the python  file tests.
@@ -10,5 +17,4 @@ for file in "tests"/*.py; do
     file=$(basename $file .py)
     echo "Testing: $file"
     runTest $file
-    # todo add check with acc. python interpreter. 
 done
